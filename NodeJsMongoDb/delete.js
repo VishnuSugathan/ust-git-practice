2e86263fff64024c2f06067601d0a613cc515ee1
unknown <vishnusugathann@gmail.com>
2025-11-28 17:37:03 +0530

    day 9 and 10

--- NodeJsMongoDb/delete.js
+++ NodeJsMongoDb/delete.js
@@ -1,6 +1,8 @@
 const { MongoClient } = require('mongodb');
 const url = "mongodb://localhost:27017/";
 
+
+
 async function createCollection() {
   try {
     const client = await MongoClient.connect(url);
